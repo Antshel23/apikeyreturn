daf9e2809f28be33dd7f8849c2de6ccdb119a22b
Antshel23 <antsheldon23@icloud.com>
2024-10-07 13:30:47 +0100

    done

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ require('dotenv').config();
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+async function getImage(query) {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`)
+    const data = await response.json()
+    const gifUrl = data.data.length > 0 ? data.data[0].images.original.url : null
+    console.log(gifUrl)
+    return gifUrl
+}
+getImage('cat')
